61273e1c6f224d98aa181ab896d98329ce22d204
Sarrah Vesselov <sarchka1@gmail.com>
2015-02-08 19:49:17 -0500

    commit this

---
+++ answers.js
@@ -0,0 +1,159 @@
+// ANSWERED
+// ---------Show me how to calculate the average price of all items. Please console.log the average.
+
+// ---------The average price is $23.63
+
+// --- to find the average of a set of numbers, add them all together and divide by the number of items 
+// --- the price of each item is contained within an object, within an array   "price": number,
+// --- map the price for each object
+// --- use the reduce function to add together all the items
+// --- divide this number by the total number of prices
+
+
+
+var getPrice = items.map(function(costValue){
+  return costValue.price;
+});
+
+var sumPrice = getPrice.reduce(function(total, item){
+  return  total + item;
+});
+
+var avPrice = "The average price is $ " + (sumPrice / 25).toFixed(2);
+
+
+// ANSWERED
+// ----------Show me how to get an array of items that cost between $14.00 and $18.00 USD
+
+/*"Items that cost between $14.00 USD and $18.00 USD:"
+[
+{
+  title: "1970s Coors Banquet Glass Beer Pitcher",
+  ...
+},
+{
+  title: "The Three Broomsticks Customizable Beer Stein Mug, Harry Potter Inspired, hogsmeade village, harry potter gift, three broomsticks mug",
+  ...
+},
+{
+  title: "Hand Painted Colorful Feather Glass",
+  ...
+}
+]
+// --- sort them by items with a price greater than/equal to  $14.00 and less than/equal to $18.00 USD
+// --- return this in an array beginning with a string that states : Items that cost between $14.00 USD and $18.00 USD:
+*/
+
+
+
+var cheap = items.filter(function(stuff){
+  return stuff.price >= 14 && stuff.price <= 18;
+});
+
+
+
+// ANSWERED
+/* Show me how find the item with a "GBP" currency code and print its name and price. 
+Please console.log the one you find.
+
+1970s Schlitz Malt Liquor Glass Beer Pitcher costs £18
+
+*/
+
+
+var getItem = items.filter(function(product){
+  return product.currency_code === "GBP";
+});
+
+var theAnswer = getItem[0].title + " £" + getItem[0].price;
+
+
+// NOT ANSWERED!!!!!!!!!!
+
+/* Show me how to find which items are made of wood. 
+Please console.log the ones you find. */
+
+// "materials" contains the wood designation for a product
+// console log the titles
+// must add "is made of wood" at the end of the sentence
+
+
+
+//returns false because items does not have wood, materials does
+
+var getList = items.filter(function(stuff){
+  return stuff.materials;
+});
+
+
+var hasWood = function(items) {
+  
+  var containsWood = false;
+  
+  items.forEach(function(itemInArray) {
+      if (itemInArray === "wood"); {
+        containsWood  = true;
+      }
+  });
+  
+  return containsWood;
+};
+
+///currently returns true when I run hasWood(items)
+
+function titlesWithWood (){
+  var itemsWithWood = items.filter(function(currentItem) {
+    var currentMaterials = currentItem.materials;
+    var containsWood = hasWood(currentMaterials);
+    return containsWood;
+  });
+
+var titles = itemsWithWood.map(function(currentItem){
+  return currentItem.title + " is made of wood";
+});
+
+return titles;
+
+}
+///currently returns EVERYTHING...which is not the answer
+
+
+// ANSWERED
+/// Show me how to find which items are made of eight or more materials. Please console.log the ones you find.
+
+// first need to access the items materials lists
+// second need to determine which items have greater than 8 materials (materials.length > 8 index spots?)
+// final output needs to be item title and the list of materials
+
+
+/// This will call all of them with title and materials list
+
+var stepOne = items.filter(function(item){
+  return item;
+});
+
+var getMat = stepOne.map(function(item){
+  return [item.title, item.materials.length];
+});
+
+var thisAnswer = getMat.filter(function(item, index){
+  return item[1] >= 8;
+})
+
+
+
+// ANSWERED
+// Show me how to calculate how many items were made by their sellers - 18 were made by their sellers
+// "who_made" is the key property value
+
+
+var manf = items.map(function(madeBy) {
+  return madeBy.who_made === "i_did";
+  });
+
+ var sum = manf.reduce(function(item, value) {
+  return item + value;
+ });
+
+var answer = sum + " were made by their sellers";
+
---
+++ excercises.js
@@ -0,0 +1,240 @@
+/**
+ *
+ */
+// ANSWERED
+// ---------------------
+// Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
+// ---------------------
+// - Someone can go into console, input two numbers max(9, 8), and will get the largest back
+// - if/ then/ else
+
+function max(numb1, numb2){
+  
+  if (numb1 > numb2) {
+    return numb1;
+
+    } else if (numb2 > numb1) {
+      return numb2;
+
+    } else {
+      return "They Are The Same";
+    }
+}
+
+
+// ANSWERED
+// ---------------------
+// Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them. (Accept three param and return the largest)
+// ---------------------
+// - Someone can go into console, input three numbers max(9, 8, 6), and will get the largest back
+// - if/ then/ else
+
+
+function maxOfThree(numb1, numb2, numb3){
+     if (numb1 > numb2 && numb1 > numb3) {
+    return numb1;
+
+    } else if (numb2 > numb1 && numb2 > numb3) {
+      return numb2;
+
+    } else if (numb3 > numb1 && numb3 > numb2) {
+      return numb3;    
+
+    } else {
+      return "They Are All The Same";
+    }
+}
+
+// ANSWERED
+// ---------------------
+// Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+// ---------------------
+/* need to give it something to compare to - an array of vowels
+                  
+                  var vowels = "aeiou".split("");
+
+                  hasVowel = false;
+
+                  vowels.forEach(function(currentVowel) {
+                    if (char === currentVowel) {
+                      hasVowel = true;
+                    }
+                    return hasVowel;
+                  });   
+
+                  /*   WHAT THE HECK IS CHAR???  Returns char undefined, I know char needs to be the value I type, 
+                  but I don't know how to get it in here.
+
+Use a switch statement instead - this seems like an easier route when you are only looking for
+a small number of matches */
+
+function isVowel(myChar) {
+
+  switch(myChar) {
+      case "a":
+          return true;
+          break;
+      case "e":
+          return true;
+          break;
+      case "i":
+          return true;
+          break;
+      case "o":
+          return true;
+      case "u":
+          return true;
+          break;
+      default:
+          return "This is not a Vowel";
+          break;
+  }
+};
+
+// ANSWERED
+// ---------------------
+// Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
+// ---------------------
+
+
+function sumAll(myArray) {
+
+  var sum = 0;
+  var index;
+
+  for (index = 0; index < myArray.length; index++) {
+      
+      sum += myArray[index];
+    }
+    return sum;
+  };
+
+ex. sumAll([1,2,3,4]) = 10
+
+
+
+function multAll(myArray) {
+
+  var mult = 1; //this would not work with 0 so I tried 1. Why is it 1 !?
+  var index;
+
+  for (index = 0; index < myArray.length; index++) {
+    
+      mult *= myArray[index];
+    }
+      return mult;
+};
+
+ex. multAll([3,5]) = 15
+
+
+
+// ANSWERED
+// ---------------------
+// Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
+// ---------------------
+
+
+function reverse(str){
+    return str.split("").reverse().join("");
+}
+
+
+// ANSWERED
+// ---------------------
+// Write a function findLongestWord() that takes an array of words and returns the length of the longest one.
+// ---------------------
+
+
+function findLongestWord(words) {
+
+  var myLgth = 0;
+  var theLongest;
+
+  for(var i=0; i < words.length; i++){
+      
+      if(words[i].length > myLgth){   /////if the word is greater than 0
+      var myLgth = words[i].length;   
+      theLongest = words[i];
+      } 
+  }
+  return (theLongest);     
+};
+
+
+//ANSWERED
+// ---------------------
+// Write a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
+// ---------------------
+// find the longest word first
+// then compare that word to the number
+// if the word is longer than the number, return it
+
+
+/*  running this results in it only comparing my number to the first word in my array...why?
+
+function filterLongWords(words, i){
+
+  var myLgth = 0;
+  var theLongest;
+
+  for(var index=0; index < words.length; index++){
+      
+      if(words[index].length > myLgth){   
+      var myLgth = words[index].length;   
+      theLongest = words[index];
+      } 
+
+      if(i < theLongest.length === true){   
+      return (theLongest); 
+      }  
+
+      else {
+      return "The number is greater";
+    }
+  }    
+};
+
+
+ex. filterLongWords(["this", "is", "tough"], 4)
+*/
+
+
+
+function filterLongWords(words, int) {
+ var length = words.length;
+ var theLongest = [];
+
+ for (index = 0; index < length; index++) {
+    if (words[index].length > int) {
+      theLongest[theLongest.length] = words[index];
+   }
+ }
+ return theLongest;
+}
+
+
+// ANSWERED
+// ---------------------
+// Write a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
+// ---------------------
+///   have a string, we want to see a frequency listing
+///   we want to transform it from  a string object into an array object "str".split(" ")
+
+
+
+function charFreq(string){
+    var frequency = {};
+    for (var index=0; index<string.length; index++) {
+        var letter = string.letterAt(index);
+    if (frequency[letter]) {
+           frequency[letter]++;
+        } else {
+           frequency[letter] = 1;
+        }
+    }
+
+    return frequency;
+};
+
+ex. charFreq("abbabcbdbabdbdbabababcbcbab")
